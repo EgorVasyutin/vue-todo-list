c9e1b93c6f6d223cd9b53fe6fdd0f48e69014e7d
Egor <vaspovas@gmail.com>
2022-09-12 17:24:17 +0300

    исправил баг с регистрацией

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,11 @@
 
     <template v-slot:footer>
       <div v-if="action === 'sign-in'" class="flex flex-col gap-16 w-full">
-        <app-button class="w-full" :disabled="!isFormValid" @click="signIn">
+        <app-button
+          class="w-full"
+          :disabled="!isFormValidSignIn"
+          @click="signIn"
+        >
           Войти
         </app-button>
         <app-button type="secondary" @click="() => (action = 'sign-up')">
@@ -40,7 +44,11 @@
       </div>
 
       <div v-else-if="action === 'sign-up'" class="flex flex-col gap-16 w-full">
-        <app-button class="w-full" :disabled="!isFormValid" @click="signUp">
+        <app-button
+          class="w-full"
+          :disabled="!isFormValidSignUp"
+          @click="signUp"
+        >
           Создать аккаунт
         </app-button>
         <app-button type="secondary" @click="() => (action = 'sign-in')">
@@ -118,17 +126,32 @@ export default {
     modalTitle() {
       return this.action === "sign-in" ? "Вход" : "Регистрация";
     },
-    isEmailValid() {
+    isEmailValidSignIn() {
       return (
         this.signInForm.email.includes(".com") ||
         this.signInForm.email.includes(".ru")
       );
     },
-    isPasswordValid() {
+    isPasswordValidSignIn() {
       return this.signInForm.password.length >= 6;
     },
-    isFormValid() {
-      return this.isEmailValid && this.isPasswordValid;
+    isFormValidSignIn() {
+      return this.isEmailValidSignIn && this.isPasswordValidSignIn;
+    },
+    isEmailValidSignUp() {
+      return (
+        this.signUpForm.email.includes(".com") ||
+        this.signUpForm.email.includes(".ru")
+      );
+    },
+    isPasswordValidSignUp() {
+      return (
+        this.signUpForm.password.length >= 6 &&
+        this.signUpForm.password === this.signUpForm.repeatPassword
+      );
+    },
+    isFormValidSignUp() {
+      return this.isEmailValidSignUp && this.isPasswordValidSignUp;
     },
   },
   methods: {
